a94ea0fdfa8cfe92d7787eaee499476bd4c5d56c
WBattist <144733159+WBattist@users.noreply.github.com>
2024-08-06 16:09:55 -0400

    Update settings.js

--- public/assets/js/settings.js
+++ public/assets/js/settings.js
@@ -497,48 +497,6 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-
-window.addEventListener("load", function () {
-  const adsToggle = document.getElementById("adsToggle");
-  const adsOn = localStorage.getItem("adsOn") === "true";
-  adsToggle.checked = adsOn;
-});
-
-var adsOn = window.localStorage.getItem("adsOn");
-var gAdsOn = window.localStorage.getItem("gAdsOn");
-
-if (adsOn === "false") {
-  console.log("User said no ads :( okkkk");
-} else {
-  const gascript = document.createElement("script");
-  gascript.setAttribute("src", "//banddisordergraceless.com/5b/d2/9b/5bd29b62af75682aa2a77e20931069ee.js");
-  document.head.append(gascript);
-  console.log("Added Advert Script");
-}
-
-
-function togglegAds() {
-  const adsToggle2 = document.getElementById("gAdsToggle");
-  const gAdsOn = adsToggle2.checked;
-  localStorage.setItem("gAdsOn", gAdsOn);
-}
-
-window.addEventListener("load", function () {
-  const adsToggle2 = document.getElementById("gAdsToggle");
-  const gAdsOn = localStorage.getItem("gAdsOn") === "true";
-  adsToggle2.checked = gAdsOn;
-});
-
-if (gAdsOn === "false") {
-  console.log("User said no google ads :( okkkk");
-} else {
-  const googleascript = document.createElement("script");
-  googleascript.setAttribute("src", "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2804638430420139");
-  googleascript.setAttribute("crossorigin", "anonymous");
-  document.head.append(googleascript);
-  console.log("Added Goggle Adsense Script");
-}
-
 document.addEventListener("DOMContentLoaded", function () {
   const navbarLinks = document.querySelectorAll(".navbarLink");
 
